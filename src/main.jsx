f34f64ab10c0b0076c9003f33bd179858029518b
codepaucode <paulajane.gonzales@gcash.com>
2024-07-31 14:18:20 +0800

    "second commit"

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
+import {createRoot} from 'react-dom/client'
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+    errorElement: <div>Page not found</div>
+  },
+  {path:"/abc",
+    element: <div>abc</div>
+  }
+])
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
